329b6f80f79bfb9888436f944a553502dbc379a2
Ionelll <serban.nelutu@gmail.com>
2024-02-18 03:24:43 +0100

    Call to action ready

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MainContentComponent } from '../../components/main-content/component/main-content.component';
 import { EventsComponent } from '../../components/events/component/events.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MainContentComponent, EventsComponent],
+  imports: [MainContentComponent, EventsComponent, RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
@@ -20,6 +21,25 @@ export class LandingPageComponent {
       document.getElementById('back-top').classList.remove('back-top');
     } else {
       document.getElementById('back-top').classList.add('back-top');
+    }
+  }
+  changeOverlay(hover: boolean, emitter: string) {
+    if (hover)
+      if (emitter === 'login')
+        document
+          .getElementById('login-img')
+          .style.setProperty('filter', 'none');
+      else
+        document
+          .getElementById('signup-img')
+          .style.setProperty('filter', 'none');
+    else {
+      document
+        .getElementById('login-img')
+        .style.setProperty('filter', 'sepia(100%)');
+      document
+        .getElementById('signup-img')
+        .style.setProperty('filter', 'sepia(100%) ');
     }
   }
 }
